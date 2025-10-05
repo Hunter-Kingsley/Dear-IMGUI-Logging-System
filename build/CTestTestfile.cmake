d3e3c955a4e7fc4585aaa17a187fbe23133779c6
Hunter-Kingsley <hunterkingsley329@gmail.com>
2025-10-05 14:25:04 -0700

    got starter window to open

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/their/Documents/GitHub/Dear-IMGUI-Logging-System
+# Build directory: C:/Users/their/Documents/GitHub/Dear-IMGUI-Logging-System/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ out/build/Visual Studio Community 2022 Release - x86_amd64/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/their/Documents/GitHub/Dear-IMGUI-Logging-System
+# Build directory: C:/Users/their/Documents/GitHub/Dear-IMGUI-Logging-System/out/build/Visual Studio Community 2022 Release - x86_amd64
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
